e57273eb4ef1609420c695c88df9c48ce7761c66
Amrutansu Mishra <amrutansu@Amrutansus-MacBook-Air.local>
2025-06-16 18:34:08 +0530

    ui redesign

--- frontend/src/components/ExpenseTable/ExpenseTable.jsx
+++ frontend/src/components/ExpenseTable/ExpenseTable.jsx
@@ -1,85 +1,45 @@
 import React,{useEffect,useState} from 'react'
-import './ExpenseTable.css'
-import 'boxicons';
-import Notification from '../Notification/Notification';
-import Pagination from '../../components/Pagination/Pagination';
-import { fetchExpense } from '../../services/services';
+import { LuShoppingBag } from "react-icons/lu";
+import { LuTrendingDown } from "react-icons/lu";
+import { LuTrash2 } from "react-icons/lu";
+import { LuPencilLine } from "react-icons/lu";
+import './ExpenseTable.css';
 // import { resultData } from '../../constants/dummyData';
 
-const ExpenseTable = () => {
-  const [expenseData,setExpenseData] = useState()
-  const [isLastPage,setIsLastPage] = useState()
-  const perPageData=2
-  const [pageNo, setPageNo] = useState(1)
-  const getExpenses = async()=>{
-    const result = await fetchExpense(pageNo*perPageData,perPageData)
-    console.log(result)
-    if(result.data.success){
-      setIsLastPage(result.data.isLastPage)
-      setExpenseData(result.data.result)
-    }
-  }
-
-  const handleFilter = (e) =>{
-    console.log(e.target.value)
-  }
-  useEffect(()=>{
-    getExpenses()
-  },[pageNo])
+const ExpenseTable = ({expenseData,children}) => {
   return (<>
-    <div className='expense-card'>
-        <div className='expense-table-card'>
-          <Notification/>
-        <div className='filter-row'>
-                <div className='filter-header'>Your Expenses</div>
-                <div className='filter-options'>
-                    <div className='action-button'>
-                    <button>
-                     <box-icon name="printer"></box-icon> 
-                </button>
-                    </div>
-                    <div className='action-button'>
-                    <button>
-                     <box-icon name="download"></box-icon> 
-                </button>
-                    </div>
-                    <div className='filter-select'>
-                        <select onChange={handleFilter}>
-                            <option value="today">Today</option>
-                            <option value="week">This Week</option>
-                            <option value="month">This month</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
             <div className='expense-table'>
+              {children}
               <div className='expense-table-content'>
-            
-            <table>
-              <thead>
-              <tr className='table-header'>
-        <th>Expense</th>
-        <th>Category</th>
-        <th>Date</th>
-        <th>Amount</th>
-      </tr>
-              </thead>
-              <tbody>   
-      {expenseData?expenseData.map((data,index)=>
-        <tr key={`expense-row-${index}`}>
-          <td>{data.name}</td>
-          <td>{data.category}</td>
-          <td>{data.date}</td>
-          <td><span>&#8377;</span>{data.amount}</td>
-        </tr>):<></>}
-      </tbody>
-    </table>
+                {expenseData?expenseData.map((data,index)=>
+                <div className="expense-rows">
+                  <div className="expense-col1">
+                    <div className="expense-logo">
+                      <LuShoppingBag size={25} />
+                    </div>
+                    <div className="expense-text">
+                    <div className="expense-title">
+                      {data.name}
+                    </div>
+                    <div className="expense-date">
+                      {data.date}
+                    </div>
+                    </div>
+                  </div>
+                  <div className="expense-col2">
+                    <div className='delete-icon'>
+                      <LuTrash2 size={22}/>
+                    </div>
+                    <div className='edit-icon'>
+                      <LuPencilLine size={22}/>
+                    </div>
+                    <div className="expense-money">
+                      - &#8377;{data.amount} <LuTrendingDown/>
+                    </div>
+                  </div>
+                </div>):<></>
+                }
     </div>
-            </div>
-        </div>
-    </div>
-    <div className='pagination-trans'>
-      <Pagination pageNo={pageNo} setPageNo={setPageNo} isLastPage={isLastPage}/>
     </div>
     </>
   )
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -2,9 +2,11 @@ import React, { useEffect } from "react";
 import Expense from "../../components/Expense/Expense";
 import ExpenseTable from "../../components/ExpenseTable/ExpenseTable";
 // import ExpenseHeader from "../../components/ExpenseHeader/ExpenseHeader";
+import { NavLink } from 'react-router-dom';
 import { fetchExpense } from '../../services/services';
 import { useStore } from "../../context/StoreProvider";
 import Layout from "../../components/Layout/Layout";
+import { LuArrowRight } from "react-icons/lu";
 import "./Dashboard.css";
 
 const Dashboard = () => {
@@ -13,7 +15,7 @@ const Dashboard = () => {
   useEffect(()=>{
     const getExpenses = async()=>{
       setLoader(true)
-      const result = await fetchExpense(0)
+      const result = await fetchExpense(1,7)
       if(result?.data?.success){
         setExpenseData(result.data.result)
       }
@@ -31,7 +33,14 @@ const Dashboard = () => {
 						<Expense/>
 						<div className='expense-card'>
 						{/* <ExpenseHeader/> */}
-						<ExpenseTable expenseData={expenseData}/>
+						<ExpenseTable expenseData={expenseData}>
+							<div className="expense-header">
+								<div className="header-text">Recent Transactions</div>
+								<NavLink to="/transaction" className="see-all-link">
+									See All <LuArrowRight/>
+								</NavLink>
+							</div>
+						</ExpenseTable>
 					</div>
 				</div>
 			</div>
